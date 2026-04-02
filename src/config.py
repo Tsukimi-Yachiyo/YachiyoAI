dc2de14edd80a7abc377b24b12f608d898f1b48a
drayee <1473443474@qq.com>
2026-04-02 22:26:22 +0800

    定义了记忆服务

--- src/config.py
+++ src/config.py
@@ -1,16 +1,20 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
+    # 阿里云百炼
     DASHSCOPE_API_KEY: str
+    # PostgresSQL
     DB_NAME: str
     DB_USER: str
     DB_PASSWORD: str
     DB_HOST: str
     DB_PORT: int
+    # 模型
     MODEL_NAME: str
     BASE_URL: str
 
-    class Config:
-        env_file = ".env"
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8")
 
 settings = Settings()
--- src/llm/model.py
+++ src/llm/model.py
@@ -1,10 +1,43 @@
 from langchain_openai import ChatOpenAI
 from src.config import settings
 
-def get_llm():
-    return ChatOpenAI(
-        model=settings.MODEL_NAME,
-        api_key=settings.DASHSCOPE_API_KEY,
-        base_url=settings.BASE_URL,
-        temperature=0.7
-    )
+
+class ModelDepository:
+
+    def __init__(self):
+        self.models = []
+
+        self.models.append(
+            ChatOpenAI(
+                model=settings.MODEL_NAME,
+                api_key=settings.DASHSCOPE_API_KEY,
+                base_url=settings.BASE_URL,
+                temperature=0.7
+            )
+        )
+
+    def add_model(self, llm_model : str,
+                  api_key : str,
+                  base_url : str,
+                  temperature : float = 0.7):
+        self.models.append(
+            ChatOpenAI(
+                model=llm_model,
+                api_key=api_key,
+                base_url=base_url,
+                temperature=temperature
+            )
+        )
+
+    def delete_model(self, name : str):
+        if name is not None:
+            for llm_model in self.models:
+                if llm_model.name == name:
+                    self.models.remove(llm_model)
+        else:
+            self.models.pop(0)
+
+    def get_llm(self):
+        return self.models[0]
+
+model = ModelDepository()
