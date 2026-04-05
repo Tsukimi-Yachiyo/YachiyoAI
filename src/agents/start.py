4c83fabd73d934e0bf7a5b8bf4b70597d5786233
drayee <1473443474@qq.com>
2026-04-05 19:54:50 +0800

    添加相关注释

--- src/agents/start.py
+++ src/agents/start.py
@@ -6,34 +6,58 @@ from .tool.summary_out import summary_out
 from langgraph.graph import END
 from .other.display import start_agent
 
+# 启动智能体
 class StartAgent:
 
+
+    """
+        启动智能体
+        :param chatLLM: 语言模型
+        :return: 智能体实例
+    """
     def __init__(self, chatLLM: ChatOpenAI):
         self.chatLLM = chatLLM
 
+        # 定义状态图
         workflow = StateGraph(State)
 
+        # 添加节点
         workflow.add_node("summary_out",summary_out)
         workflow.add_node("review",review)
 
-
+        # 设置入口点
         workflow.set_entry_point("review")
 
+        # 添加条件边
         workflow.add_conditional_edges(
             "review",
             self.should_continue
         )
 
+        # 编译状态图
         self.app = workflow.compile()
 
+    """
+        绘制状态图
+    """
     def get_graph(self):
         start_agent(self.app)
 
+    """
+        获取当前状态图实例
+        :return: 状态图实例
+    """
     def get_state(self):
         return self.app
 
+    """
+        判断是否继续执行
+        :param state: 状态
+        :return: 是否继续执行
+    """
     @staticmethod
     def should_continue(state):
         if state["stop"]:
             return True
         return False
+
--- src/agents/state.py
+++ src/agents/state.py
@@ -1,6 +1,7 @@
 from typing import TypedDict, Annotated, Sequence
 from langchain_core.messages import BaseMessage
 
+# 状态定义
 class State(TypedDict):
     messages: Annotated[Sequence[BaseMessage], "add"]
     need_reflect: bool
